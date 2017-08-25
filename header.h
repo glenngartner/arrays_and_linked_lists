0be87a58c235169acc156124fa756b2ec137fc09
Glenn Gartner <ggartner@sonalysts.com>
2017-08-25 09:54:35 -0400

    add linked item creation, linking, and value printing helper functions

--- header.h
+++ header.h
@@ -1,11 +1,19 @@
-void printSize(int value);
-
-void printValue(int value);
+#include <string>
 
 struct LinkedListItem
 {
+	std::string name;
 	int value;
 	LinkedListItem *next;
 };
 
+void printSize(LinkedListItem value);
+
+void printValue(LinkedListItem item);
+
+
 void printMemoryAddress(LinkedListItem item);
+
+LinkedListItem makeListItem(std::string name);
+
+void linkItems(LinkedListItem first, LinkedListItem second);
